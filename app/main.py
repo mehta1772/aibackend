cac4bc43590d287d985fcc91cd7f266cc3504dfd
Mehta <purushottam.1772@gmail.com>
2026-01-30 16:19:01 +0530

    fix cors issue

--- app/main.py
+++ app/main.py
@@ -39,7 +39,7 @@ origins = [
     "http://localhost:3000",
     "http://localhost:5173",
     "https://crm.enigoal.in",  # Update with your Hostinger domain
-    "https://www.enigoal.in",
+    "https://enigoal.in",
 ]
 
 app.add_middleware(
